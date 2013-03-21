2776dca726e65f59c773eaa6e169dec64e6ba0dd
ivanhanigan <ivanhanigan@gmail.com>
2013-03-21 14:21:09 +1100

    update vlist

--- R/variableslist.r
+++ R/variableslist.r
@@ -1,17 +1,16 @@
 
-###########################################################################
+#####################################################################
 # newnode: variableslist
 variableslist <- function()
   {
   variablesList<-"variable,measure,timestep
-  rainfall,totals,daily
-  temperature,maxave,daily
-  temperature,minave,daily
-  vprp,vprph09,daily
-  vprp,vprph15,daily
-  solar,solarave,daily
-  ndvi,ndviave,month
-  "
+rainfall,totals,daily
+temperature,maxave,daily
+temperature,minave,daily
+vprp,vprph09,daily
+vprp,vprph15,daily
+solar,solarave,daily
+ndvi,ndviave,month"
   variablesList <- read.csv(textConnection(variablesList))
   return(variablesList)
   }
