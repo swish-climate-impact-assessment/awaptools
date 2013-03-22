5eb3a193cf440ebc2aed6814da86f8e29742afe4
ivanhanigan <ivanhanigan@gmail.com>
2013-03-22 12:01:16 +1100

    rasteraggregate fix

--- R/raster_aggregate.r
+++ R/raster_aggregate.r
@@ -4,7 +4,7 @@
 raster_aggregate <- function(filename, aggregationfactor, delete = TRUE)
 {
   r <- raster(filename)
-  r <- aggregate(r, fact = aggregationfactor, fun = mean)
+  if(aggregationfactor > 1) r <- aggregate(r, fact = aggregationfactor, fun = mean)
   writeRaster(r, gsub('.grid','',fname), format="GTiff",
 overwrite = TRUE)
   if(delete)
