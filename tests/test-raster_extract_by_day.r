013093d2fb80413e711e7df19e949b270b8d2c18
ivanhanigan <ivanhanigan@gmail.com>
2013-04-25 15:15:15 +1000

    add mod to test

--- tests/test-raster_extract_by_day.r
+++ tests/test-raster_extract_by_day.r
@@ -1,17 +1,16 @@
 
 ################################################################
 # name:raster_extract_by_day
-# test raster extract
 require(swishdbtools)
 conn <- connect2postgres2("ewedb")
 raster_extract_by_day(
   ch = conn
   , 
-  startdate = '2013-04-19'
+  startdate = '2013-04-18'
   , 
   enddate = '2013-04-20'
   , 
-  measures = c("maxave", "minave")
+  measures = c("maxave", "minave", "vprph09", "vprph15")
   ,
   schemaName = "weather_sla"
   ,
