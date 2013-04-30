ffc2aa2fda8f57f96c5f82f15af943b19ff50251
Ivan <ivan.hanigan@gmail.com>
2013-04-30 12:08:07 +1000

    some fixes from ian work

--- R/raster_extract_by_day.r
+++ R/raster_extract_by_day.r
@@ -5,17 +5,18 @@ raster_extract_by_day  <- function(ch, startdate, enddate,
                                    schemaName = "weather_sla",
                                    tableName = "weather_nswsla06",
                                    pointsLayer = "abs_sla.nswsla06_points",
-                                   measures = c("maxave", "minave")
-                                   )
+                                   measures = c("maxave", "minave"),
+                                   zone_label = "address"
+)
 {
-   
+  
   dates <- as.character(
     seq(
       as.Date(startdate),
       as.Date(enddate), 1
-      )
     )
-   
+  )
+  
   for(date_j in dates)
   {
     tblExists <- pgListTables(ch,"public", "tempfoobar")
@@ -28,7 +29,7 @@ raster_extract_by_day  <- function(ch, startdate, enddate,
     {
       try(dbSendQuery(ch, sprintf("drop table  %s.%s", schemaName, tableName)))
     }
-       
+    
     date_i <- gsub("-","",date_j)
     #print(date_i)
     for(i in 1:length(measures))
@@ -39,46 +40,51 @@ raster_extract_by_day  <- function(ch, startdate, enddate,
       #tableExists <- pgListTables(ch, schema="awap_grids", table=paste(measure, "_", date_i, sep =""))
       #if(nrow(tableExists) > 0)
       #{
-        sql <- postgis_raster_extract(ch, x=rastername, y=pointsLayer, zone_label = "sla_code", value_label = "value")
-        sql <- gsub("FROM", "INTO public.tempfoobar\nFROM", sql)
-        #cat(sql)  
-        
-        dbSendQuery(ch, sql) 
-        
-        tblExists <- pgListTables(ch, schemaName, tableName)
-        if(nrow(tblExists) == 0)
-        {
+      sql <- postgis_raster_extract(ch, x=rastername, y=pointsLayer, zone_label = zone_label, value_label = "value")
+      sql <- gsub("FROM", "INTO public.tempfoobar\nFROM", sql)
+      #cat(sql)  
+      
+      dbSendQuery(ch, sql) 
+      
+      tblExists <- pgListTables(ch, schemaName, tableName)
+      if(nrow(tblExists) == 0)
+      {
         sql <- sql_subset_into(ch, x="public.tempfoobar", into_schema=schemaName,
                                into_table=tableName,eval=F, drop=F
-                               )
+        )
         #cat(sql)
         dbSendQuery(ch, sql)      
-        } else {
-          sql <- sql_subset(ch, x="public.tempfoobar", eval=F)
-          sql <- paste("INSERT INTO ",schemaName,".",tableName," (
-          sla_code, raster_layer, value)
+      } else {
+        sql <- sql_subset(ch, x="public.tempfoobar", eval=F)
+        sql <- paste("INSERT INTO ",schemaName,".",tableName," (
+          ", zone_label, ", raster_layer, value)
           ",sql,sep ="")
-          #cat(sql)
-          dbSendQuery(ch, sql)
-        }
-        dbSendQuery(ch, "drop table public.tempfoobar")
+        #cat(sql)
+        dbSendQuery(ch, sql)
+      }
+      dbSendQuery(ch, "drop table public.tempfoobar")
       #}
     }
   }
 }
 
-reformat_awap_data  <- function(
-  tableName = "weather_sla.weather_nswsla06"
-  )
-  {
-dat <- sql_subset(ch, tableName, eval = T)
-dat$date <- matrix(unlist(strsplit(dat$raster_layer, "_")), ncol = 3, byrow=TRUE)[,3]
-dat$date <- paste(substr(dat$date,1,4), substr(dat$date,5,6), substr(dat$date,7,8), sep = "-")
-dat$measure <- matrix(unlist(strsplit(dat$raster_layer, "_")), ncol = 3, byrow=TRUE)[,2]
-dat$measure <- gsub("grids.","",dat$measure)
 
-dat <- arrange(dat,  date, measure)
-dat <- as.data.frame(cast(dat, sla_code + date ~ measure, value = "value"))
-dat$date <- as.Date(dat$date)
-return(dat)
-  }
+
+
+
+reformat_awap_data  <- function(
+  tableName = "weather_sla.weather_nswsla06",
+  zone_label = "address"
+)
+{
+  dat <- sql_subset(ch, tableName, eval = T)
+  dat$date <- matrix(unlist(strsplit(dat$raster_layer, "_")), ncol = 3, byrow=TRUE)[,3]
+  dat$date <- paste(substr(dat$date,1,4), substr(dat$date,5,6), substr(dat$date,7,8), sep = "-")
+  dat$measure <- matrix(unlist(strsplit(dat$raster_layer, "_")), ncol = 3, byrow=TRUE)[,2]
+  dat$measure <- gsub("grids.","",dat$measure)
+  
+  dat <- arrange(dat,  date, measure)
+  dat <- as.data.frame(cast(dat, address + date ~ measure, value = "value"))
+  dat$date <- as.Date(dat$date)
+  return(dat)
+}
--- tests/test-raster_extract_by_day.r
+++ tests/test-raster_extract_by_day.r
@@ -2,24 +2,34 @@
 ################################################################
 # name:raster_extract_by_day
 require(swishdbtools)
-conn <- connect2postgres2("ewedb")
-raster_extract_by_day(
-  ch = conn
-  , 
-  startdate = '2013-04-18'
-  , 
-  enddate = '2013-04-20'
-  , 
-  measures = c("maxave", "minave", "vprph09", "vprph15")
-  ,
-  schemaName = "weather_sla"
-  ,
-  tableName = "weather_nswsla06"
-  ,
-  pointsLayer = "abs_sla.nswsla06_points"
+require(awaptools)
+startdate <- "2013-04-01" #StartDate
+enddate <- "2013-04-02" #EndDate
+
+#source("D:\\Development\\SydneyThing\\raster_extract_by_day.R")
+
+ch<-connect2postgres2("ewedb")
+
+tempTableName <- tempfile("foo", tmpdir = Sys.getenv("TEMP"), fileext = "")
+
+tempTableName <- gsub("\\.", "", tempTableName)
+tempTableName <- gsub(":", "", tempTableName)
+tempTableName <- gsub("\\\\", "", tempTableName)
+tempTableName <- gsub("/", "", tempTableName)
+
+raster_extract_by_day(ch, startdate, enddate,
+                                   schemaName = "public",
+                                   tableName = tempTableName,
+                                   pointsLayer = "locations",
+                                   measures = c("maxave", "minave")
 )
 
-dat <- reformat_awap_data(
-  tableName = "weather_sla.weather_nswsla06"
+schemaTableName <- paste(sep=".", "public", tempTableName)
+
+data <- reformat_awap_data(
+  tableName = schemaTableName
 )
-head(dat)
+
+tempFileName <- tempfile("foo", tmpdir = Sys.getenv("TEMP"), fileext = "")
+write.dta(data, tempFileName)
+tempFileName
