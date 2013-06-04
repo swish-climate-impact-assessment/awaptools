d48e4bf40bcb17cc4a53d04a71ecea8060a3acf2
ivanhanigan <ivan.hanigan@gmail.com>
2013-06-04 12:38:01 +1000

    add conn argument to raster_extract_by_day

--- R/raster_extract_by_day.r
+++ R/raster_extract_by_day.r
@@ -1,7 +1,7 @@
 
 ################################################################
 # name:raster_extract_by_day
-raster_extract_by_day  <- function(ch, startdate, enddate,
+raster_extract_by_day  <- function(ch = NA, startdate = NA, enddate = NA,
                                    schemaName = "weather_sla",
                                    tableName = "weather_nswsla06",
                                    pointsLayer = "abs_sla.nswsla06_points",
@@ -23,22 +23,22 @@ raster_extract_by_day  <- function(ch, startdate, enddate,
     ################################################
     # ad hoc table "public", "tempfoobar"
     temporary_table <- swish_temptable()
-    tblExists <- pgListTables(ch,temporary_table$schema, 
+    tblExists <- pgListTables(conn = ch, temporary_table$schema, 
                               temporary_table$table
                               )
     if(nrow(tblExists) >0)
     {
-      dbSendQuery(ch, sprintf("drop table %s", temporary_table$fullname))
+      dbSendQuery(conn = ch, sprintf("drop table %s", temporary_table$fullname))
     }
     #date_j <- dates[2]
     ################################################
     # the output table to append into, if exists on day one then remove
     if(date_j == dates[1])
     {
-      tblExists <- pgListTables(ch,schemaName,tableName)
+      tblExists <- pgListTables(conn = ch,schemaName,tableName)
       if(nrow(tblExists) >0)
       {
-      dbSendQuery(ch, sprintf("drop table  %s.%s", schemaName, tableName))
+      dbSendQuery(conn = ch, sprintf("drop table  %s.%s", schemaName, tableName))
       }
     }
     
@@ -52,7 +52,7 @@ raster_extract_by_day  <- function(ch, startdate, enddate,
       #tableExists <- pgListTables(ch, schema="awap_grids", table=paste(measure, "_", date_i, sep =""))
       #if(nrow(tableExists) > 0)
       #{
-      sql <- postgis_raster_extract(ch, x=rastername, 
+      sql <- postgis_raster_extract(conn = ch, x=rastername, 
                                     y=pointsLayer, 
                                     zone_label = zone_label, 
                                     value_label = "value"
@@ -64,26 +64,26 @@ raster_extract_by_day  <- function(ch, startdate, enddate,
                   sql)
       #cat(sql)  
       
-      dbSendQuery(ch, sql) 
+      dbSendQuery(conn = ch, statement = sql) 
       
-      tblExists <- pgListTables(ch, schemaName, tableName)
+      tblExists <- pgListTables(conn = ch, schemaName, tableName)
       if(nrow(tblExists) == 0)
       {
-        sql <- sql_subset_into(ch, x=temporary_table$fullname, 
+        sql <- sql_subset_into(conn = ch, x=temporary_table$fullname, 
                                into_schema=schemaName,
                                into_table=tableName,eval=F, drop=F
         )
         # cat(sql)
-        dbSendQuery(ch, sql)      
+        dbSendQuery(conn = ch, sql)      
       } else {
-        sql <- sql_subset(ch, x=temporary_table$fullname, eval=F)
+        sql <- sql_subset(conn = ch, x=temporary_table$fullname, eval=F)
         sql <- paste("INSERT INTO ",schemaName,".",tableName," (
           ", zone_label, ", raster_layer, value)
           ",sql,sep ="")
         #cat(sql)
-        dbSendQuery(ch, sql)
+        dbSendQuery(conn = ch, sql)
       }
-      dbSendQuery(ch, sprintf("drop table %s", temporary_table$fullname))
+      dbSendQuery(conn = ch, sprintf("drop table %s", temporary_table$fullname))
       #}
     }
   }
