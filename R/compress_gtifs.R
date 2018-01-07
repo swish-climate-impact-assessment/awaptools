3b6499920530955da9fc17f05a318b79b4367dda
ivanhanigan <ivan.hanigan@gmail.com>
2018-01-07 23:55:03 +1100

    add mod args to compress gtif

--- R/compress_gtifs.R
+++ R/compress_gtifs.R
@@ -1,29 +1,18 @@
 
-compress_gtifs <- function(indir = getwd()){
-
-# load some, plot to check, and demonstrate conversion to GeoTIF
-cfiles <- stack(dir(pattern = "grid$"))
-
-#plot(cfiles, nc = 2) # plot to look at
-
-# Check if GTif/AWAP directory exists, it not, create it (taken from http://stackoverflow.com/questions/4216753/check-existence-of-directory-and-create-if-doesnt-exist)
-
-subDir <- "GTif"
-
-if (file.exists(file.path(indir, subDir))){
+compress_gtifs2 <- function (indir = getwd(), subDir = "GTif", filelist = NULL){
+  if(is.null(filelist)) stop("no list of grids provided")
+  cfiles <- stack(filelist)
+  
+  if (file.exists(file.path(indir, subDir))) {
     setwd(file.path(indir, subDir))
-} else {
+  }
+  else {
     dir.create(file.path(indir, subDir))
     setwd(file.path(indir, subDir))
-
-}
-
-# write grid files as GeoTif files to disk with compression
-# to save space
-writeRaster(cfiles, filename = "GTif", format = "GTiff",
-            bylayer = TRUE, overwrite = TRUE, suffix = "names",
-            dataType = "INT2S", options = c("COMPRESS=LZW"))
-setwd(indir)
-print("done")
-  
+  }
+  writeRaster(cfiles, filename = "GTif", format = "GTiff", 
+              bylayer = TRUE, overwrite = TRUE, suffix = "names", dataType = "INT2S", 
+              options = c("COMPRESS=LZW"))
+  setwd(indir)
+  print("done")
 }
